23c42456477eeac90985c0742bb13c3f93673966
Arnaud DON <donnadille.arnaud@gmail.com>
2021-08-17 11:13:29 +0200

    Un composant qui affiche une alert avec bonjour

--- TPgrp/src/app/app.module.ts
+++ TPgrp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposantArnaudComponent } from './composant-arnaud/composant-arnaud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposantArnaudComponent
   ],
   imports: [
     BrowserModule,
